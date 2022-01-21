8442206e67df1e0dc8656921dae2c40e6ca8d923
vega005 <vega005@interia.pl>
2022-01-21 20:39:37 +0100

    A few changes.

--- src/main/java/com/example/ProjektJava/ProjektJava/BookRepository.java
+++ src/main/java/com/example/ProjektJava/ProjektJava/BookRepository.java
@@ -3,11 +3,9 @@ package com.example.ProjektJava.ProjektJava;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    //List<Book> findByType(BookType bookType);
 
 }
